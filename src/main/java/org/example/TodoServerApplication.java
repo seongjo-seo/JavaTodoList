e197d79ef9198cdc816128b59ab9a9bf1aad2411
seoseongjo <tjtjdwh5321@gmail.com>
2022-12-23 17:05:04 +0900

    intellij commit practice and TodoService

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,8 +1,11 @@
 package org.example;
 
-public class TodoServerApplication {
-    public static void main(String[] args){
-        System.out.println("HELLO TODO");
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+public class TodoServerApplication {
+    @SpringBootApplication
+    public static void main(String[] args){
+        SpringApplication.run(TodoServerApplication.class, args);
     }
 }
