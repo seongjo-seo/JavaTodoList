b0810e044a889b1f1f00efbdab05a7f2b459e76a
seoseongjo <tjtjdwh5321@gmail.com>
2023-01-03 15:33:22 +0900

    서버어플리케이션 위치 조정 및 컨트롤러 기능 추가

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -3,8 +3,8 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class TodoServerApplication {
-    @SpringBootApplication
     public static void main(String[] args){
         SpringApplication.run(TodoServerApplication.class, args);
     }
